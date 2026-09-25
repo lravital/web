d3f76d8c149c823ad212636863661c95a6d262e9
lravital <lravital@gmail.com>
2026-09-25 23:45:11 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,22 +1,59 @@
 const SITE_DATA = {
-  siteTitle: "My Teaching Resources",
-  grades: [
-    { name: "Grade 7", categories: [
-      { name: "General Resources", links: [] },
-      { name: "Materials", links: [] }
-    ]},
-    { name: "Grade 8", categories: [
-      { name: "General Resources", links: [] },
-      { name: "Atoms & Elements", links: [] }
-    ]},
-    { name: "Grade 9", categories: [
-      { name: "General Resources", links: [] },
-      { name: "Chemistry", links: [] }
-    ]},
-    { name: "General Resources", categories: [
-      { name: "Examples", links: [
-        { name: "Toyota Corolla – real-life car example", url: "https://www.yad2.co.il/vehicles/item/88roj7po#galleryModal-grid-item-0", description: "Real-life vehicle example." }
-      ]}
-    ]}
+  "siteTitle": "My Teaching Resources",
+  "grades": [
+    {
+      "name": "Grade 7",
+      "categories": [
+        {
+          "name": "General Resources",
+          "links": [
+            {
+              "name": "מערכת הדם סרטון סיכום קצר",
+              "url": "https://www.youtube.com/watch?v=u1LQKm-pKFE",
+              "description": ""
+            }
+          ]
+        },
+        {
+          "name": "Materials",
+          "links": []
+        }
+      ]
+    },
+    {
+      "name": "Grade 8",
+      "categories": [
+        {
+          "name": "General Resources",
+          "links": []
+        },
+        {
+          "name": "Atoms & Elements",
+          "links": []
+        }
+      ]
+    },
+    {
+      "name": "Grade 9",
+      "categories": [
+        {
+          "name": "General Resources",
+          "links": []
+        },
+        {
+          "name": "Chemistry",
+          "links": []
+        }
+      ]
+    },
+    {
+      "name": "General Resources",
+      "categories": [
+        {
+          "name": "Examples",
+          "links": []
+        }
+      ]
+    }
   ]
 };
