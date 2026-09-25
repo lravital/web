5932d64479e79691ed5a6e0b573d5a1156309ff9
lravital <lravital@gmail.com>
2026-09-25 23:11:53 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const RESOURCE_DATA = {
+  siteTitle: "My Teaching Resources",
+  grades: [
+    {
+      id: "grade-7",
+      name: "Grade 7",
+      categories: [
+        { id: "general", name: "General", links: [{ id: "link-yad2", name: "Toyota Corolla – real-life car example", url: "https://www.yad2.co.il/vehicles/item/88roj7po#galleryModal-grid-item-0", description: "Real-life vehicle example." }] }
+      ]
+    },
+    {
+      id: "grade-8",
+      name: "Grade 8",
+      categories: [
+        { id: "general", name: "General", links: [] }
+      ]
+    },
+    {
+      id: "grade-9",
+      name: "Grade 9",
+      categories: [
+        { id: "general", name: "General", links: [] }
+      ]
+    }
+  ]
+};
